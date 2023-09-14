9111b27c308285fba18637e5ba143b3dd2fcf644
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-09-14 23:26:31 +0200

    privwent curah

--- src/components/EmailVerification.tsx
+++ src/components/EmailVerification.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row, Form, Button, Alert, Spinner } from 'react-bootstrap';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import logo from "../assets/favicon.png"
@@ -11,25 +11,28 @@ import { RootState } from '../redux/store';
 
 const EmailVerification: React.FC = () => {
   const params = useParams<{ user_id: string,user_name:string }>();
-
-  const isLoading = useSelector((state: any) => state.emailVerification.isLoading);
-  const isError = useSelector((state: any) => state.emailVerification.isError);
+  const isLoading = useSelector((state: RootState) => state.verifyEmail.isLoading);
+  const isError = useSelector((state: RootState) => state.verifyEmail.isError);
   const emailVerificationResponse = useSelector((state:RootState ) => state.verifyEmail.data);
   const [signUp, setSignUp] = useState<boolean>(false);
   const [response, setResponse] = useState<boolean>(false);
   const dispatch:Dispatch<any> = useDispatch();
 
-  console.log(emailVerificationResponse, 'ME');
+ 
   
 
   const handleVerification = async () => {
-    setSignUp(true);
-   
-    const user_id = params.user_id || ''; 
-   
-    await dispatch(emailVerification(user_id)); 
-    setResponse(true);
+    try {
+      setSignUp(true);
+  
+      const user_id = params.user_id || '';
+      await dispatch(emailVerification(user_id));
+      setResponse(true);
+    } catch (error) {
+      console.error('Error during email verification:', error);
+    }
   };
+  
   const navigate=useNavigate()
   
   return (
@@ -44,38 +47,45 @@ const EmailVerification: React.FC = () => {
                   <Icon.XLg className="textColor" size={20} />
                 </Link>{' '}
               </div>
+              <hr />
 
               {isLoading && signUp && (
-                <div className="  d-flex justify-content-center">
-                  <Spinner  className='spinner-border-sm me-2' />
-                </div>
+                  <div className="  d-flex justify-content-center">
+                    <Spinner className='spinner-border-sm me-2' />
+                  </div>
+                )}
+                {isError && (
+                  <Alert variant="danger" className="mt-5">
+                    <Alert.Heading>!You got an error!</Alert.Heading>
+                    <small>Something went wrong on our side, we are working on it, we apologize for the inconvenience caused</small>
+                  </Alert>
+                )}
+                {response && emailVerificationResponse && !isError && (
+                  <Alert variant="primary">{emailVerificationResponse.message}</Alert>
               )}
-              {isError && (
-                <Alert variant="danger" className="mt-5">
-                  <Alert.Heading>!You got an error!</Alert.Heading>
-                  <small>Something went wrong on our side, we are working on it, we apologies for the inconvenience caused</small>
-                </Alert>
-              )}
-              {response && !isError && <Alert variant="primary">{emailVerificationResponse.data.message}</Alert>}
-              <hr />
+
             </Col>
           </Row>
           <Row>
             <Col>
               <Form.Group className="d-flex mb-4">
                 <Col>
-                  <Form.Text>
-                    <strong className='text-white'>Hey {params.user_name}! Please verify your email.</strong>
-                  </Form.Text>
                   <div>
                     {response && !isError?(
+
                   <Button variant="primary" onClick={()=>{navigate("/")}} className="w-50 my-5 content_bg-2">
                     Explore more
                   </Button>
+                       
                     ):(
+                        <div className='d-flex flex-column justify-content-center align-items-center'>
+                  <Form.Text>
+                    <strong className='text-white'>Hey {params.user_name}! Please verify your email.</strong>
+                  </Form.Text>
                   <Button onClick={handleVerification} variant="primary" className="w-50 my-5 content_bg-2">
                     Verify email
                   </Button>
+                  </div> 
                     )}
                   </div>
                   <Link className="mr-auto" to={'/'}>
