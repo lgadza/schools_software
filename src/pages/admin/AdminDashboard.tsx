7e7eb3355c9e432a699518d4916778003c2b1d46
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-09-30 15:56:39 +0200

    admition

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -17,10 +17,21 @@ const AdminDashboard: React.FC = () => {
             </div>
         </Row>
         <Row>
+        <Col sm={6} md={3}>
+            <div className="d-flex justify-content-between align-items-center px-3 py-2 content_bg">
+                <ul>
+                    <li className="text-start">Application Window</li>
+                    <li className="text-start">Range here</li>
+                </ul>
+                <div>
+                    <FontAwesomeIcon icon={faUser}/>
+                </div>
+            </div>
+            </Col>
             <Col sm={6} md={3}>
             <div className="d-flex justify-content-between align-items-center px-3 py-2 content_bg">
                 <ul>
-                    <li className="text-start">Students</li>
+                    <li className="text-start">Total Applications</li>
                     <li className="text-start"><strong>4059</strong></li>
                 </ul>
                 <div>
@@ -31,18 +42,7 @@ const AdminDashboard: React.FC = () => {
             <Col sm={6} md={3}>
             <div className="d-flex justify-content-between align-items-center px-3 py-2 content_bg">
                 <ul>
-                    <li className="text-start">Department</li>
-                    <li className="text-start"><strong>30+</strong></li>
-                </ul>
-                <div>
-                    <FontAwesomeIcon icon={faUser}/>
-                </div>
-            </div>
-            </Col>
-            <Col sm={6} md={3}>
-            <div className="d-flex justify-content-between align-items-center px-3 py-2 content_bg">
-                <ul>
-                    <li className="text-start">Applications</li>
+                    <li className="text-start">Total Valid Applications</li>
                     <li className="text-start"><strong>300</strong></li>
                 </ul>
                 <div>
@@ -53,7 +53,7 @@ const AdminDashboard: React.FC = () => {
             <Col sm={6} md={3}>
             <div className="d-flex justify-content-between align-items-center px-3 py-2 content_bg">
                 <ul>
-                    <li className="text-start">Students</li>
+                    <li className="text-start">Total Enrolled</li>
                     <li className="text-start"><strong>4059</strong></li>
                 </ul>
                 <div>
