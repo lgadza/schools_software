374613220f0fc8f4905929229fdb5659fafd5ae4
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-10-03 11:36:10 +0200

    adress

--- src/pages/admissionsManagement/student/account/Adress.tsx
+++ src/pages/admissionsManagement/student/account/Adress.tsx
@@ -242,6 +242,14 @@ e.preventDefault()
         )}
       <option value="Bulawayo">Bulawayo</option>
       <option value="Harare">Harare</option>
+      <option value="Mashonaland Central">Mashonaland Central</option>
+      <option value="Mashonaland East">Mashonaland East</option>
+      <option value="Mashonaland West">Mashonaland West</option>
+      <option value="Manicaland">Manicaland</option>
+      <option value="Midlands">Midlands</option>
+      <option value="Masvingo">Masvingo</option>
+      <option value="Matabeleland North">Matabeleland North</option>
+      <option value="Matabeleland South">Matabeleland South</option>
     </Form.Control>
   
         </Col>
