b7d40845b253a623b5b77858b022ff1832b6b279
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-10-07 18:25:16 +0200

    selected schools

---
+++ src/pages/admissionsManagement/student/account/Application.tsx
@@ -0,0 +1,67 @@
+import { Card, Col, Form, Row } from "react-bootstrap"
+import { gradesAndForms } from "../../../../assets/data/gradesAndForms"
+import Select from "react-select";
+
+const Application=():JSX.Element=>{
+ 
+    return(
+        <div>
+    <h5 className="d-flex my-3">Application</h5>
+
+{/* <MultipleSelect/> */}
+<Form >
+      <Row>
+        <Col>
+        <Form.Label className="d-flex">Level <span className="text-danger">*</span></Form.Label>
+          <Form.Select
+           required 
+        //    value={guardian.level}
+            // onChange={handleChange}
+            >
+                <option>select level</option>
+                {   
+                    gradesAndForms.map((level:{value:string,label:string})=>{
+                        return(
+                            <option value={level.value}>{level.label}</option>
+                            )})
+                }
+            </Form.Select>
+        </Col>
+        <Col>
+        <Form.Label className="d-flex">
+              Select School <span className="text-danger">*</span>
+            </Form.Label>
+            <Select
+              required
+              options={gradesAndForms.map((level) => ({
+                value: level.value,
+                label: level.label,
+              }))}
+              placeholder="Select school"
+              isMulti={true}
+            />
+            <div className="steps-bar mt-4">
+              <h6>No application available</h6>
+            </div>
+            <Card className="main_bg my-3">
+                <Card.Title className="text-start">
+                    Nkulumani High
+                </Card.Title>
+                <Card.Body>
+                    <Card.Text className="text-start">
+                        Location: Bulawayo, Nkulumani 6
+                    </Card.Text>
+                    <Card.Subtitle className="text-start text-muted">
+                        Applied: Form 1
+                    </Card.Subtitle>
+                </Card.Body>
+
+            </Card>
+        </Col>
+      </Row>
+    </Form>
+
+    </div>
+    )
+}
+export default Application
